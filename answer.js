575d4c551a72920aa0f024865f2ded260dfb5d5c
jwelchman <j.welchman@yahoo.com>
2019-11-20 16:08:25 -0700

    finished challenge 2

--- answer.js
+++ answer.js
@@ -1,1 +1,12 @@
 // *** YOUR ANSWER BELOW ***
+
+let inputA = 9;
+let inputB = 4;
+
+function quotient(inputA, inputB){
+    let answer = Math.floor(inputA / inputB);
+    let remainder = Math.floor(inputA % inputB);
+    return answer + " " + "r" + remainder;
+}
+
+console.log(quotient(9,4));
